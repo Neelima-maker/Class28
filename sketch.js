505791e0999cbcff2325b84b4e2558d49e1c0380
Neelima-maker <mneelimarani@gmail.com>
2021-05-31 22:16:18 +0530

    platform added

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var platform,fixedlog , chain1,sling1;
+var platform1
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,9 +18,9 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-    
-    ground = new Ground(600,height,1200,20)
-
+    platform1 = new Ground(160,350,30,190);
+    ground = new Ground(600,400,1200,20)
+    platform = new Ground(150,310,300,180);
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -28,13 +31,13 @@ function setup(){
     pig3 = new Pig(810, 220);
 
     log3 =  new Log(810,180,300, PI/2);
-
+    
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    fixedlog = new Log(230,180,80,PI/2);
     bird = new Bird(100,100);
-
+    sling1 = new slingshot(bird.body ,{x:200, y:100});
 }
 
 function draw(){
@@ -59,4 +62,15 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
+    platform1.display();
+    fixedlog.display();
+    sling1.display();
+
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body ,{x:mouseX, y:mouseY})
+}
+function mouseReleased(){
+    sling1.fly();
 }
